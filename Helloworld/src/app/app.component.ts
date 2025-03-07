9e46c646496ca09b01c32c7dd82060dbea0cf5c7
Aayush501 <shrivastavashu501@gmail.com>
2025-03-07 15:39:00 +0530

    [Aayush Kumar Shrivastav] Add . UC5 -> Using Input Event Binding -> Solution

--- Helloworld/src/app/app.component.ts
+++ Helloworld/src/app/app.component.ts
@@ -12,6 +12,7 @@ export class AppComponent {
   title = 'Helloworld';
   Image = "assets/BL_logo_square_jpg.jpg";
   userName: string = "";
+  nameError: string = "";
 
   ngOnInit() {
     this.title = "Hello From Bridgelabz";
@@ -19,5 +20,15 @@ export class AppComponent {
 
   openBridgeLabz() {
     window.open("https://www.bridgelabz.com/", "_blank");
+  }
+
+  onInput($event : any) {
+    console.log("Change Event Occurred!", $event.data);
+    const nameRegex = RegExp("^[A-Z]{1}[A-Za-z\\s]{2,}$");
+    if(nameRegex.test(this.userName)){
+      this.nameError = "";
+      return;
+    }
+    this.nameError = "Name is incorrect !!!";
   }
 }
